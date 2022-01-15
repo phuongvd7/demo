5677b06afab3f91b5c8ae60fffc1d672073e6de2
phuongvd7 <70727122+phuongvd7@users.noreply.github.com>
2022-01-15 16:12:27 +0700

    commit b1b2 tieptuc

---
+++ Buoi2/src/hinhhoc/MainTinh.java
@@ -0,0 +1,12 @@
+package hinhhoc;
+
+public class MainTinh {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		System.out.println("DT HV = " +  HinhVuong.dienTich(a)); // goi bang ten class.tenham
+
+	}
+// overload thi co nghia la dat ten ham con trung ten deu duoc nhung co the khac kieu bien truyen vao
+}
