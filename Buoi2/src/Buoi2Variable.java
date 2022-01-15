413421f01f8bc8591c60c0f9cd4da515aedd05cf
phuongvd7 <70727122+phuongvd7@users.noreply.github.com>
2022-01-15 16:12:41 +0700

    Create Buoi2Variable.java

---
+++ Buoi2/src/Buoi2Variable.java
@@ -0,0 +1,15 @@
+
+public class Buoi2Variable {
+
+
+ 	//b1: khai bao ham
+	//ctrl + shift + f
+	public static void tong(){// nghia la ham nay khong can chuyen vao gi het
+		// tuong tu ham main
+		int a = 10;
+		int b = 20;
+		int tong = a + b;
+		System.out.println(tong);
+		
+	}
+}
